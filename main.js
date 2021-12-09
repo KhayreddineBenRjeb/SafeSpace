aeebedba72fa5c4040409e440d76cb8b181de743
unknown <brkhayreddine@gmail.com>
2021-12-09 21:10:11 +0100

    almost done

---
+++ main.js
@@ -0,0 +1,6 @@
+var button=$("<button>publish<button>")
+button.addClass("#publish")
+$(".publish").click(function(){
+array=[]
+array.push($("#textarea").val())
+$(".illPushH").append("<div>"+array[array.length-1]+"</div>")})
